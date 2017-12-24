932156e30c7cc1db8f687bedb595db6c71ebc70a
Frizy-up <yuanzefengde@163.com>
2017-12-24 04:28:48 -0800

    add tanh functtion

--- picturePlot.py
+++ picturePlot.py
@@ -5,7 +5,8 @@ import random
 import math
 
 
-
+'''
+# plot sigmoid function
 x = np.arange(-10,10,0.001)
 # sigmoid = [i*i for i in x]
 sigmoid = 1. / ( 1 + np.exp(-x) )
@@ -24,3 +25,23 @@ pl.legend()
 # pl.ylabel(u"sf")
 pl.show()
 fig.savefig('sigmoid.png')
+
+'''
+# plot tanh function
+x = np.arange(-10,10,0.001)
+# sigmoid = [i*i for i in x]
+tanh = (np.exp(x) - np.exp(-x)) / ( np.exp(x) + np.exp(-x) )
+tanhDer = 1 - pow(tanh,2)
+
+fig = pl.figure()
+pl.grid(True)
+pl.xlim(-10, 10)
+pl.ylim(-1, 1)
+# pl.title('Sigmoid')
+pl.plot(x, tanh, 'r-', label='tanh function')
+pl.plot(x, tanhDer, 'b--', label='Derivative of tanh function')
+pl.legend()
+# pl.xlabel('')
+# pl.ylabel(u"sf")
+pl.show()
+fig.savefig('tanh.png')
