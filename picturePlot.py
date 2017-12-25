7cbe0a19b4c6f2ff2975c0c7ca3872983d67ea33
Frizy-up <yuanzefengde@163.com>
2017-12-25 04:33:53 -0800

    plot elu

--- picturePlot.py
+++ picturePlot.py
@@ -76,6 +76,8 @@ pl.show()
 fig.savefig('relu.png')
 '''
 
+
+'''
 mpl.rcParams['axes.unicode_minus']=False
 
 fig = plt.figure(figsize=(6,4))
@@ -102,7 +104,63 @@ plt.legend()
 plt.show()
 fig.savefig('relu.png')
 
+'''
+
+'''
+# plot leak ReLU function
+mpl.rcParams['axes.unicode_minus']=False
+
+fig = plt.figure(figsize=(6,4))
+ax = fig.add_subplot(111)
+
+x = np.arange(-10, 10)
+y = np.where(x<0,0.2*x,x)
+
+plt.xlim(-11,11)
+plt.ylim(-11,11)
+
+ax.spines['top'].set_color('none')
+ax.spines['right'].set_color('none')
+
+ax.xaxis.set_ticks_position('bottom')
+ax.spines['bottom'].set_position(('data',0))
+ax.set_xticks([-10,-5,0,5,10])
+ax.yaxis.set_ticks_position('left')
+ax.spines['left'].set_position(('data',0))
+ax.set_yticks([-10,-5,5,10])
+
+plt.plot(x,y,label="Leak ReLU",color = "red")
+plt.legend()
+plt.show()
+fig.savefig('leakRelu.png')
+
+'''
 
 
 
+# plot ELU function
+mpl.rcParams['axes.unicode_minus']=False
 
+fig = plt.figure(figsize=(6,4))
+ax = fig.add_subplot(111)
+
+x = np.arange(-10, 10)
+y = np.where(x<0,1*(np.exp(x)-1),x)
+
+plt.xlim(-11,11)
+plt.ylim(-11,11)
+
+ax.spines['top'].set_color('none')
+ax.spines['right'].set_color('none')
+
+ax.xaxis.set_ticks_position('bottom')
+ax.spines['bottom'].set_position(('data',0))
+ax.set_xticks([-10,-5,0,5,10])
+ax.yaxis.set_ticks_position('left')
+ax.spines['left'].set_position(('data',0))
+ax.set_yticks([-10,-5,5,10])
+
+plt.plot(x,y,label="ELU",color = "red")
+plt.legend()
+plt.show()
+fig.savefig('ELU.png')
